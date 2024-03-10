bc4cc868328fcd6f0576d9ad55a5483af73d624d
jbotgil <javibotella05@gmail.com>
2024-03-10 11:19:52 +0100

    2024-03-10
    Reto #3 - La sucesión de Fibonacci

---
+++ src/Reto3Fibonacci/jbotgil.java
@@ -0,0 +1,29 @@
+package Reto3Fibonacci;
+
+public class jbotgil {
+
+    /*
+     * Escribe un programa que imprima los 50 primeros números de la sucesión
+     * de Fibonacci empezando en 0.
+     * - La serie Fibonacci se compone por una sucesión de números en
+     *   la que el siguiente siempre es la suma de los dos anteriores.
+     *   0, 1, 1, 2, 3, 5, 8, 13...
+     *
+     * @author jbotgil
+     */
+
+    public static void main(String[] args) {
+        //Utiliazamos el tipo long ya que vamos a trabajar con cantidades de numeros grandes
+        long numeroPrevio = 0;
+        long siguienteNumero = 1;
+
+        System.out.println("50 primeros numeros de la sucesion de Fibonacci: ");
+        for (int i = 0; i < 50; ++i) {
+            System.out.print(numeroPrevio + " ");
+
+            long sum = numeroPrevio + siguienteNumero;
+            numeroPrevio = siguienteNumero;
+            siguienteNumero = sum;
+        }
+    }
+}
